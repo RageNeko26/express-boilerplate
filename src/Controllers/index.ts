939a682296547aee83c9bc7e17315c104e6ba292
RageNeko26 <rneko2006@gmail.com>
2023-09-16 22:04:09 +0700

    fix: skipping params in function

--- src/Controllers/index.ts
+++ src/Controllers/index.ts
@@ -1,30 +1,30 @@
 import { Request, Response } from "express"
 
-export const Home = (req: Request, res: Response): void => {
+export const Home = (_req: Request, res: Response): void => {
   res.json({
     message: "Welcome to Boilerplate API Version 1.0.0"
   })
 }
 
-export const About = (req: Request, res: Response ): void => {
+export const About = (_req: Request, res: Response ): void => {
   res.json({
     message: "About route"
   })
 }
 
-export const Nested = (req: Request, res: Response): void => {
+export const Nested = (_req: Request, res: Response): void => {
   res.json({
     message: "I'm nested"
   })
 }
 
-export const OtherNest = (req: Request, res: Response): void => {
+export const OtherNest = (_req: Request, res: Response): void => {
   res.json({
     message: "Other nest route"
   })
 }
 
-export const v1 = (req: Request, res: Response): void => {
+export const v1 = (_req: Request, res: Response): void => {
   res.json({
     message: "Home version 1.0.0"
   })
